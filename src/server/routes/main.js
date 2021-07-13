5413eafc711560e4f909c26669ea6e7e17bf260b
thaiphong1101 <tuthaiphong600@gmail.com>
2021-07-13 16:19:39 +0700

    active user, sortByRole, sortByPlan

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { createNewUser, deleteUser, getAllUser, UpdateUser } from '../controllers/User.js';
+import { createNewUser, deleteUser, getAllUser, updateUser, activeUser, sortUserByRole, sortUserByPlan } from '../controllers/User.js';
 const router = express.Router();
 router.post('/createUser', createNewUser);
 router.get('/getUser', getAllUser);
-router.patch('/updateUser/:userId', UpdateUser);
+router.patch('/updateUser/:userId', updateUser);
 router.delete('/deleteUser/:userId', deleteUser);
+router.patch('/activeUser/:userId', activeUser);
+router.post('/sortRole', sortUserByRole);
+router.post('/sortPlan', sortUserByPlan);
+
 
 export default router;
